947f2942bd2c6ed6f4ad9deab857fb0faa86ed3d
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-09-12 22:08:01 +0200

    Set icons set & put SVGs in src folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ async function fetchData(location) {
   const apiKey = 'YXSUVQ3DB4TJWPLWYEPNXTRC2';
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?iconSet=icons2&key=${apiKey}`,
     );
     const data = await response.json();
     console.log(data);
