68e98463eabadbecf2e40c2e4fa85b975254610c
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-09-12 21:19:36 +0200

    Set API query

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import "./modern-normalize.css";
-import "./style.css";
+import './modern-normalize.css';
+import './style.css';
+
+async function fetchData(location) {
+  const apiKey = 'YXSUVQ3DB4TJWPLWYEPNXTRC2';
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`,
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData('głowaczów');
