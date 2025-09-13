7e257b2de5a821228909afab092fc2b3e381f332
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-09-13 21:37:02 +0200

    Put fetchData into separated module

---
+++ src/fetchData.js
@@ -0,0 +1,30 @@
+export async function fetchData(location) {
+  const apiKey = 'YXSUVQ3DB4TJWPLWYEPNXTRC2';
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?iconSet=icons2&key=${apiKey}`,
+    );
+    if (!response.ok) {
+      throw new Error('Could not fetch data');
+    }
+    const data = await response.json();
+    console.log(data);
+    // Return filtered data
+    return {
+      address: data.resolvedAddress,
+      day: data.days[0].datetime,
+      icon: data.currentConditions.icon,
+      temperature: data.currentConditions.temp,
+      wind: data.currentConditions.windspeed,
+      sunrise: data.currentConditions.sunrise,
+      sunset: data.currentConditions.sunset,
+      future: data.days.slice(1, 8).map((day) => ({
+        day: day.datetime,
+        icon: day.icon,
+        temperature: day.temp,
+      })),
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
 import './modern-normalize.css';
 import './style.css';
+import { fetchData } from './fetchData';
 
-async function fetchData(location) {
-  const apiKey = 'YXSUVQ3DB4TJWPLWYEPNXTRC2';
-  try {
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?iconSet=icons2&key=${apiKey}`,
-    );
-    if (!response.ok) {
-      throw new Error('Could not fetch data');
-    }
-    const data = await response.json();
-    console.log(data);
-    // Return Promise with filtered data
-    return {
-      address: data.resolvedAddress,
-      day: data.days[0].datetime,
-      icon: data.currentConditions.icon,
-      temperature: data.currentConditions.temp,
-      wind: data.currentConditions.windspeed,
-      sunrise: data.currentConditions.sunrise,
-      sunset: data.currentConditions.sunset,
-      future: data.days.slice(1, 8).map((day) => ({
-        day: day.datetime,
-        icon: day.icon,
-        temperature: day.temp,
-      })),
-    };
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-fetchData('ahlbeck').then((weather) => console.log(weather));
+fetchData('syndey').then((weather) => console.log(weather));
