af2c838cf3e65ab195dce255d54fe9e4a101c7f1
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-09-19 23:20:42 +0200

    Add conditions appending with toogle recognizing

--- src/appendForecast.js
+++ src/appendForecast.js
@@ -14,6 +14,9 @@ export function appendForecast(data) {
     if (div.classList.contains('day-length')) {
       dayLength(data, div);
     }
+    if (div.classList.contains('conditions')) {
+      conditions(data, div);
+    }
   });
 }
 
@@ -128,6 +131,93 @@ function getMinutes(time) {
 }
 
 function getDayLength(minutes) {
-  return `${Math.floor(minutes / 60).toString().padStart(2, '0')}:${(minutes % 60).toString().padStart(2, '0')}`;
+  return `${Math.floor(minutes / 60)
+    .toString()
+    .padStart(2, '0')}:${(minutes % 60).toString().padStart(2, '0')}`;
 }
 
+async function conditions(data, div) {
+  const tempUnitToggle = document.getElementById('temp-unit-toggle-box');
+  const windUnitToggle = document.getElementById('wind-unit-toggle-box');
+
+  // Temperatue
+  const tempSpan = document.createElement('span');
+  div.appendChild(tempSpan);
+  const tempIconScr = await import('./assets/info_icons/thermometer.svg');
+  const tempIcon = document.createElement('img');
+  tempIcon.alt = 'temperature';
+  tempIcon.src = tempIconScr.default;
+  tempSpan.appendChild(tempIcon);
+  tempSpan.appendChild(document.createElement('br'));
+  const tempValue = document.createElement('span');
+  tempValue.classList.add('temp-value');
+  // Fill based on toggle state
+  tempSpan.appendChild(tempValue);
+  const tempUnit = document.createElement('span');
+  tempUnit.classList.add('temp-unit');
+  // Fill based on toggle state
+  tempSpan.appendChild(tempUnit);
+
+  div.appendChild(document.createElement('br'));
+
+  // Wind speed
+  const windSpan = document.createElement('span');
+  div.appendChild(windSpan);
+  const windIconScr = await import('./assets/info_icons/wind_speed.svg');
+  const windIcon = document.createElement('img');
+  windIcon.alt = 'wind speed';
+  windIcon.src = windIconScr.default;
+  windSpan.appendChild(windIcon);
+  windSpan.appendChild(document.createElement('br'));
+  const windValue = document.createElement('span');
+  windValue.classList.add('wind-value');
+  // Fill based on toggle state
+  windSpan.appendChild(windValue);
+  const windUnit = document.createElement('span');
+  windUnit.classList.add('wind-unit');
+  // Fill based on toggle state
+  windSpan.appendChild(windUnit);
+
+  div.appendChild(document.createElement('br'));
+
+  // Humidity
+  const humiditySpan = document.createElement('span');
+  div.appendChild(humiditySpan);
+  const humidityIconScr = await import('./assets/info_icons/droplet.svg');
+  const humidityIcon = document.createElement('img');
+  humidityIcon.alt = 'humidity';
+  humidityIcon.src = humidityIconScr.default;
+  humiditySpan.appendChild(humidityIcon);
+  humiditySpan.appendChild(document.createElement('br'));
+  humiditySpan.appendChild(document.createTextNode(`${data.humidity}%`));
+
+  div.appendChild(document.createElement('br'));
+
+  // Pressure
+  const pressureSpan = document.createElement('span');
+  div.appendChild(pressureSpan);
+  const pressureconScr = await import('./assets/info_icons/arrow-down.svg');
+  const pressureIcon = document.createElement('img');
+  pressureIcon.alt = 'pressure';
+  pressureIcon.src = pressureconScr.default;
+  pressureSpan.appendChild(pressureIcon);
+  pressureSpan.appendChild(document.createElement('br'));
+  pressureSpan.appendChild(document.createTextNode(`${data.pressure} hPa`));
+
+  // Checking toggle state
+  if (!tempUnitToggle.checked) {
+    tempValue.textContent = Math.round(data.temperature);
+    tempUnit.textContent = ' °C';
+  } else if (tempUnitToggle.checked) {
+    tempValue.textContent = Math.round((data.temperature * 9) / 5 + 32);
+    tempUnit.textContent = ' °F';
+  }
+
+  if (!windUnitToggle.checked) {
+    windValue.textContent = Math.round(data.wind);
+    windUnit.textContent = ' km/h';
+  } else if (windUnitToggle.checked) {
+    windValue.textContent = Math.round(data.wind * 0.621371);
+    windUnit.textContent = ' mph';
+  }
+}
