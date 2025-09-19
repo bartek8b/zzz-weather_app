5d82c291077799a5ca7b97801ace37fa230c2991
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-09-19 19:33:11 +0200

    Add date0 fill

--- src/appendForecast.js
+++ src/appendForecast.js
@@ -8,6 +8,9 @@ export function appendForecast(data) {
     if (div.classList.contains('icon')) {
       weatherIcon(data, div);
     }
+    if (div.classList.contains('date-0')) {
+      date(data, div);
+    }
   });
 }
 
@@ -42,4 +45,40 @@ async function weatherIcon(data, div) {
   img.style.width = '70%';
 
   div.appendChild(img);
+}
+
+async function date(data, div) {
+  const src = await import('./assets/info_icons/calendar.svg');
+  const icon = document.createElement('img');
+  icon.alt = 'date';
+  icon.src = src.default;
+  div.appendChild(icon);
+
+  div.appendChild(document.createElement('br'));
+
+  getDateAndDay(data.day, div);
+}
+
+function getDateAndDay(dateString, div) {
+  const dateSpan = document.createElement('span');
+  dateSpan.textContent = dateString;
+  div.appendChild(dateSpan);
+
+  div.appendChild(document.createElement('br'));
+
+  const date = new Date(dateString);
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const day = days[date.getDay()];
+
+  const daySpan = document.createElement('span');
+  daySpan.textContent = day;
+  div.appendChild(daySpan);
 }
